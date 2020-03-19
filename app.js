5ed2407ca89d18f0f2f6b2924383905ed7efb6f4
Fabio Dias <mfabiodias@gmail.com>
2020-03-18 21:35:04 -0300

    Projeto Iniciado V1.0

---
+++ app.js
@@ -0,0 +1,98 @@
+/* importar as configurações do servidor */
+const app = require('./config/server');
+
+
+
+// const port = process.env.PORT || 8080;
+/* parametrizar a porta de escuta */
+// const server = app.listen(port, function(){
+// 	console.log('Servidor online na porta '+port);
+// })
+
+const PORT = process.env.PORT || 3000;
+const INDEX = '/app.js';
+
+const server = app
+.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+.listen(PORT, function(){
+	console.log('Servidor online na porta '+PORT);
+})
+
+const io = require('socket.io').listen(server);
+
+app.set('io', io);
+
+/* criar a conexão por websocket */
+io.on('connection', function(socket){
+	console.log('Usuário conectou');
+
+	socket.on('disconnect', function(){
+		console.log('Usuário desconectou');
+	});
+
+	socket.on('msgParaServidor', function(data){
+
+		if(data.mensagem.toLowerCase() == 'ola')
+		{
+			socket.emit(
+				'msgParaCliente', 
+				{apelido: data.apelido, mensagem: data.mensagem}
+			);
+
+			socket.emit(
+				'msgParaCliente', 
+				{apelido: 'Robo', mensagem: 'Oi, tudo bem? Estou em desenvolvimento e em breve estarei pronto para te ajudar.'}
+			);
+		}
+		else if(data.mensagem.toLowerCase() == 'pagar')
+		{
+			socket.emit(
+				'msgParaCliente', 
+				{apelido: data.apelido, mensagem: data.mensagem}
+			);
+
+			socket.emit(
+				'msgParaCliente', 
+				{apelido: 'Robo', mensagem: 'Espertinho! Deseja pagar porque estou em fase de teste e ainda não consigo gerar seu boleto ne!? kkkkkk'}
+			);
+		}
+		else
+		{
+			socket.emit(
+				'msgParaCliente', 
+				{apelido: data.apelido, mensagem: data.mensagem}
+			);
+
+			socket.emit(
+				'msgParaCliente', 
+				{apelido: 'Robo', mensagem: 'Olá, sou o Robo do SemParar e ainda estou em fase de testes.<br>Digite alguma das mensagens abaixo para testar meu bot<br><br>1-) Ola<br>2-) Pagar'}
+			);
+		}
+
+		// /* Mensagem enviada e impressa para mim mesmo */
+		// socket.emit(
+		// 	'msgParaCliente', 
+		// 	{apelido: data.apelido, mensagem: data.mensagem}
+		// );
+
+		// /* Mensagem enviada e impressa para demais membros */
+		// socket.broadcast.emit(
+		// 	'msgParaCliente', 
+		// 	{apelido: data.apelido, mensagem: data.mensagem}
+		// );
+
+		// /* participantes */
+		// if(parseInt(data.apelido_atualizado_nos_clientes) == 0){
+		// 	socket.emit(
+		// 		'participantesParaCliente', 
+		// 		{apelido: data.apelido}
+		// 	);
+
+		// 	socket.broadcast.emit(
+		// 		'participantesParaCliente', 
+		// 		{apelido: data.apelido}
+		// 	);
+		// }
+	});
+
+});
---
+++ app/controllers/chat.js
@@ -0,0 +1,28 @@
+module.exports.iniciaChat = function(application, req, res){
+	
+	const dadosForm = req.body;
+
+	console.log(dadosForm);
+
+	req.assert('apelido','Nome ou apelido é obrigatório').notEmpty();
+	req.assert('apelido','Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+	
+	const erros = req.validationErrors();
+
+	if(erros){
+		res.render("index", {validacao : erros})
+		return;
+	}
+
+	// application.get('io').on(
+	// 	'msgParaServidor',
+	// 	console.log(dadosForm.mensagem)
+	// )
+
+	// application.get('io').emit(
+	// 	'msgParaCliente',
+	// 	{apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
+	// )
+
+	res.render("chat", {dadosForm : dadosForm});
+}
